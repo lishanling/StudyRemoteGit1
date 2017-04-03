db2aaaeb9cfe858c8646cc1dc1eafa9925b762bc
ht9495 <1441964048@qq.com>
2017-04-03 22:01:31 +0800

    添加UsersServiceImpl中的内容
    Signed-off-by: ht9495 <1441964048@qq.com>

--- mynovel/src/main/java/com/yc/novel/service/impl/UsersServiceImpl.java
+++ mynovel/src/main/java/com/yc/novel/service/impl/UsersServiceImpl.java
@@ -2,20 +2,19 @@ package com.yc.novel.service.impl;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-
-
 import org.springframework.stereotype.Service;
 
+import com.yc.novel.entity.PaginationBean;
 import com.yc.novel.entity.Users;
 import com.yc.novel.mapper.UsersMapper;
 import com.yc.novel.service.UsersService;
 
 @Service("usersService")
 public class UsersServiceImpl implements UsersService{
-	
+
 	@Autowired
 	private  UsersMapper usersMapper;
-	
+
 	@Override
 	public Users login(Users user ) {
 		   return usersMapper.find(user);
@@ -24,6 +23,24 @@ public class UsersServiceImpl implements UsersService{
 	@Override
 	public Users register(Users user) {
 		return usersMapper.registerUser(user);
+	}
+
+	@Override
+	public PaginationBean<Users> listPartUsers(String page, String rows) {
+		PaginationBean<Users> pb = new PaginationBean<Users>();
+		if(page != null){
+			pb.setCurrPage(Integer.parseInt(page));
+		}
+		if(rows != null){
+			pb.setPageSize(Integer.parseInt(rows));
+		}
+		return usersMapper.findPartUsers(pb);
+	}
+
+	@Override
+	public boolean modifyUser(Users users) {
+
+		return usersMapper.updateUser(users) > 0;
 	}
 
 
