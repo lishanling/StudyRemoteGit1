5ffa66a504aaa9d6603de46294aa5171a9629ad2
ht9495 <1441964048@qq.com>
2017-04-03 22:02:18 +0800

    添加UserService 中的内容
    Signed-off-by: ht9495 <1441964048@qq.com>

--- mynovel/src/main/java/com/yc/novel/service/UsersService.java
+++ mynovel/src/main/java/com/yc/novel/service/UsersService.java
@@ -1,7 +1,13 @@
 package com.yc.novel.service;
 
+import com.yc.novel.entity.PaginationBean;
 import com.yc.novel.entity.Users;
+
 public interface UsersService {
-	 Users login(Users user);
-	 Users register(Users user);
+	Users login(Users user);
+	Users register(Users user);
+
+	PaginationBean<Users> listPartUsers(String page, String rows);
+
+	boolean modifyUser(Users user);
 }
