3233a67d7f8c44150ad92a29de634446e155e3cd
ht9495 <1441964048@qq.com>
2017-04-05 19:45:40 +0800

    获取后台用户信息
    Signed-off-by: ht9495 <1441964048@qq.com>

--- mynovel/src/main/webapp/js/backuser.js
+++ mynovel/src/main/webapp/js/backuser.js
@@ -1,32 +1,15 @@
 $('#user_info').datagrid({    
-		url:'user/list',
-		pagination:true,
-        fitColumns:true,
-        singleSelect:true,
-    	fit:true,
-    	border:false,
-    	loadMsg:'数据加载中,请稍后...',
-    	columns:[[ 
-        {field:'usid',title:'编号',width:50 ,align:'center'},    
-        {field:'uname',title:'用户名',width:100,align:'center'} ,
-        {field:'uemail',title:'邮箱',width:100,align:'center'} ,
-        ]]
+	url:'user/list',
+	fitColumns:true,
+	fit:true,
+	singleSelect:true,
+	border:false,
+	pagination:true,
+	loadMsg:'数据加载中,请稍后...',
+	columns:[[ 
+	          {field:'usid',title:'编号',width:50 ,align:'center'},    
+	          {field:'uname',title:'用户名',width:100,align:'center'} ,
+	          {field:'uemail',title:'邮箱',width:100,align:'center'} 
+	          ]],
 });
-
-$("#modifyDiv").dialog({
-	title:"用户修改",
-	closable:false,
-	modal:true,
-});
-
-$("#modifyDiv").dialog("close");
-
-$("#userDetail").dialog({
-	title:"用户详情",
-	closable:false,
-	modal:true,
-});
-
-$("#userDetail").dialog("close");
-
 
